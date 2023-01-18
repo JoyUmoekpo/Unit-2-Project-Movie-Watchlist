8a399c2bd176033aa5e4e7d584ff381fe17235c6
joyumoekpo <joyumoekpo1@gmail.com>
2023-01-18 12:12:58 -0600

    Created and imported Header component

--- movie-watchlist/src/App.js
+++ movie-watchlist/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Header from "./components/Header";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  	const [movie, setMovie] = useState("");
+	const [watchlist, setMovielist] = useState("");
+	const [page, setPage] = useState("");
+
+	const getData = () => {
+		axios
+			.get(
+				`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=${page}`
+			)
+			.then((res) => {
+				console.log(res.data.results);
+				setMovieList(res.data.results);
+			});
+	};
+
+	useEffect(() => {
+		getData();
+	}, [page]);
+
+	return (
+		<div className="App">
+			<Header />
+		</div>
+	);
 }
 
 export default App;
