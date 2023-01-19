545828bb256b3f27a567961f0e1a8560d1a5d349
joyumoekpo <joyumoekpo1@gmail.com>
2023-01-19 12:08:46 -0600

    Final changes

--- movie-watchlist/src/App.js
+++ movie-watchlist/src/App.js
@@ -12,7 +12,7 @@ function App() {
 	const [page, setPage] = useState(1);
 
 	const addMovie = (movie) => {
-		setWatchList(...watchList, movie);
+		setWatchList([...watchList, movie]);
 	};
 
 	const removeMovie = (movie) => {
